d07d24e10c243d2385f9c18c506dab051af32816
shivamvr <shivamweb2@gmail.com>
2023-12-04 20:11:17 +0530

    minor changes

--- routes/contact.Routes.js
+++ routes/contact.Routes.js
@@ -12,9 +12,9 @@ contactRouter.use(cors());
 contactRouter.post("/add", async (req, res) => {
     const { name, email, phone, label } = req.body
     try {
-            const contact = new ContactModel({ name, email, phone,label})
-            await contact.save()
-            res.status(200).send({ "msg": "A new contact added" })
+        const contact = new ContactModel({ name, email, phone, label })
+        await contact.save()
+        res.status(200).send({ "msg": "A new contact added" })
     } catch (err) {
         console.log("Error:", err)
     }
@@ -32,6 +32,42 @@ contactRouter.get('/', async (req, res) => {
         res.send({ "Error": err })
     }
 })
+
+
+// Patch
+
+contactRouter.patch("/update/:id", async (req, res) => {
+    const payload = req.body
+    const id = req.params.id
+    console.log(id)
+    console.log(req.body)
+    try {
+        await ContactModel.findByIdAndUpdate(id, payload)
+        res.status(200).send({ "msg": "Contact updated" })
+    } catch (err) {
+        res.status(400).send({ "msg": "Contact not found" })
+    }
+})
+
+
+// Delete Contact
+
+contactRouter.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    try {
+        const contact = await ContactModel.findOne({ _id: id })
+        await ContactModel.findByIdAndDelete(id)
+        res.status(200).send({ "msg": "Contact has been successfully deleted" })
+    } catch (err) {
+        res.status(400).send({ "msg": "Contact not found" })
+    }
+})
+
+
+
+
+
 
 
 module.exports = { contactRouter }
